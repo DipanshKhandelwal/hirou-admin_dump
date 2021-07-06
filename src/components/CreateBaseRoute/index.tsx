f40937ac5abd8c253a8bef1f998e1eb48f956987
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-07-06 12:31:45 +0530

    Add flow for updating sequence and revert on cancel

--- src/components/CreateBaseRoute/index.tsx
+++ src/components/CreateBaseRoute/index.tsx
@@ -5,38 +5,92 @@ import {
   Text,
   Flex,
   Center,
+  useToast,
 } from "@chakra-ui/react"
 import { IBaseRoute } from "../../models/baseRoute"
 import { _baseRoute } from "../../store/selectors/BaseRoute"
 import { useSelector } from "react-redux"
 import { _selectedRouteId } from "../../store/selectors/App"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { ICollectionPoint } from "../../models/collectionPoint"
 import { DragDropContext, Droppable } from "react-beautiful-dnd"
 import { CollectionPointListItem } from "./components/CollectionPointListItem"
+import { UpdateConfirmationModal } from "./components/UpdateConfirmationModal"
+import { useEffect } from "react"
+import { hirouAxios } from "../../services/httpInstance"
+import { BASE_ROUTE_URL } from "../../constants/urls"
+import { dispatchUpdateBaseRoute } from "../../store/dispatcher/BaseRoute"
 
 export const CreateBaseRoute = () => {
+  const [isSeqUpdateModalOpen, setIsSeqUpdateModalOpen] = useState(false)
+  const [localCollectionPoints, setLocalCollectionPoints] = useState<ICollectionPoint[]>([])
+  const cancelRef = React.useRef()
+  const toast = useToast()
+
   const baseRoutesData: any = useSelector(_baseRoute)
   const selectedRouteId: number = useSelector(_selectedRouteId)
+
 
   const route: IBaseRoute = useMemo(() => {
     const baseRoute = baseRoutesData.data.find((baseRoute: IBaseRoute) => baseRoute.id === selectedRouteId)
     return baseRoute
   }, [baseRoutesData, selectedRouteId])
 
-  const collectionPoints = useMemo(() => {
-    return route?.collection_point.sort((a: ICollectionPoint, b: ICollectionPoint) => {
+  useEffect(() => {
+    const cps = route?.collection_point.sort((a: ICollectionPoint, b: ICollectionPoint) => {
       return a.sequence - b.sequence
     })
+    setLocalCollectionPoints(cps)
   }, [route])
 
-  const onDragEnd = (e: any) => console.log("onDragEnd", e)
+  const reorder = (list: ICollectionPoint[], startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
 
-  const collectionPointsList = (
-    collectionPoints?.map((collectionPoint: ICollectionPoint, index: number) => (
-      <CollectionPointListItem collectionPoint={collectionPoint} index={index} />
-    ))
-  )
+    return result;
+  };
+
+  const onDragEnd = (e: any) => {
+    if (!e.destination) return
+    const items = reorder(
+      localCollectionPoints,
+      e.source.index,
+      e.destination.index
+    );
+    setLocalCollectionPoints(items)
+    setIsSeqUpdateModalOpen(true)
+  }
+
+  const onSeqUpdateModalClose = () => {
+    const cps = route?.collection_point.sort((a: ICollectionPoint, b: ICollectionPoint) => {
+      return a.sequence - b.sequence
+    })
+    setLocalCollectionPoints(cps)
+    setIsSeqUpdateModalOpen(false)
+  }
+
+  const onSeqUpdate = async () => {
+    try {
+      const list = localCollectionPoints.map(cp => cp.id)
+      const url = `${BASE_ROUTE_URL}${route.id}/reorder_points/`
+      const data = { points: list }
+      const response = await hirouAxios.post(url, data);
+      const _data = response.data;
+      dispatchUpdateBaseRoute(_data)
+    } catch (e) {
+      toast({
+        title: "Error updating sequence",
+        description: "please try again",
+        status: "error",
+      })
+    }
+    setIsSeqUpdateModalOpen(false)
+  }
+
+  const collectionPointsList = localCollectionPoints?.map((collectionPoint: ICollectionPoint, index: number) => (
+    <CollectionPointListItem key={collectionPoint.id} collectionPoint={collectionPoint} index={index} />
+  ))
 
   return (
     <Box minWidth='1000px' height='inherit' >
@@ -60,6 +114,7 @@ export const CreateBaseRoute = () => {
           <Text>Map</Text>
         </Center>
       </Flex>
+      <UpdateConfirmationModal onAccept={onSeqUpdate} cancelRef={cancelRef} onCancel={onSeqUpdateModalClose} isOpen={isSeqUpdateModalOpen} />
     </Box >
   )
 }
