f97b48d50ef191b3ab061df09320cb860c02ef2e
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-07-12 23:01:00 +0530

    Add editCollectionPoint API request

--- src/services/apiRequests/collectionPoint.ts
+++ src/services/apiRequests/collectionPoint.ts
@@ -10,3 +10,18 @@ export async function saveCollectionPoint(data: any): Promise<any> {
     throw Error('Create Collection Point API failed');
   }
 }
+
+export async function editCollectionPoint(
+  cpId: number,
+  data: any
+): Promise<any> {
+  if (!cpId) throw Error('Please provide collection point id');
+
+  try {
+    const url = `${COLLECTION_POINT_URL}${cpId}/`;
+    const response = await hirouAxios.put(url, data);
+    return response.data;
+  } catch (e) {
+    throw Error('Update Collection Point API failed');
+  }
+}
