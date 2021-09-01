b553f46f045755ca16530e729ca4ca381f974ad6
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-09-02 00:26:53 +0530

    remove unused breakcrumbs

--- src/apps/baseRoute.tsx
+++ src/apps/baseRoute.tsx
@@ -7,32 +7,12 @@ import {
 } from 'react-router-dom';
 import { BaseRouteList } from '../components/BaseRouteList';
 import { CreateBaseRoute } from '../components/CreateBaseRoute';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-
-} from "@chakra-ui/react"
 
 function BaseRoute() {
   let match = useRouteMatch();
 
-  const breadcrumbs = (
-    <Breadcrumb width='max-content' padding={6} paddingY={2} fontWeight="medium" fontSize="sm" >
-      <BreadcrumbItem>
-        <BreadcrumbLink href='/base-routes' >ルート一覧</BreadcrumbLink>
-      </BreadcrumbItem>
-      {/* {selectedRouteId && (
-        <BreadcrumbItem >
-          <BreadcrumbLink href="#">{route?.name}</BreadcrumbLink>
-        </BreadcrumbItem>
-      )} */}
-    </Breadcrumb>
-  )
-
   return (
     <div>
-      {breadcrumbs}
       <Switch>
         <Route path={`${match.path}/list`}>
           <BaseRouteList />
