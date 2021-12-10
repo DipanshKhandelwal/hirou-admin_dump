1f9731bf2544975abe704ba96b796578641924f1
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-12-10 13:10:31 +0530

    Update task amount list: add taskamountItem list with task amounts

--- src/components/TaskAmountList/index.tsx
+++ src/components/TaskAmountList/index.tsx
@@ -53,34 +53,39 @@ export const TaskAmountList = ({ amountsList }: { amountsList: ITaskAmount[] }) 
         <Heading size='lg' textAlign='start' >Amounts</Heading>
         <Button onClick={() => { }} >Add Amounts</Button>
       </HStack>
-      <Table size="sm" variant='simple' >
-        <Thead>
-          <Tr>
-            <Th>S No.</Th>
-            <Th>Id</Th>
-            <Th>Type</Th>
-            <Th>Timestamp</Th>
-            <Th>Amount</Th>
-            <Th>Memo</Th>
-          </Tr>
-        </Thead>
-        <Tbody >
-          {amountsList?.map((taskAmount: ITaskAmount, idx: number) => (
-            <Tr
-              key={taskAmount.id}
-              _hover={{ backgroundColor: 'blue.100', cursor: 'pointer' }}
-              onClick={() => setSelectedTaskAmount(taskAmount)}
-            >
-              <Td>{idx + 1}</Td>
-              <Td>{taskAmount.id}</Td>
-              <Td>{taskAmount.garbage.name}</Td>
-              <Td>{taskAmount.timestamp}</Td>
-              <Td>{taskAmount.amount}</Td>
-              <Td>{taskAmount.memo}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+
+      {amountsList?.map((taskAmount: ITaskAmount, idx: number) => (
+        <>
+          <Table border='solid 2px' borderColor='blue.600' size="sm" variant='unstyled' bgColor='blue.100' >
+            <Thead>
+              <Tr>
+                {/* <Th>S No.</Th> */}
+                <Th>Id</Th>
+                <Th>Vehicle</Th>
+                <Th>Timestamp</Th>
+                <Th>Work Type</Th>
+                <Th>Deal Type</Th>
+              </Tr>
+            </Thead>
+            <Tbody >
+              <Tr
+                key={taskAmount.id}
+                _hover={{ backgroundColor: 'blue.100', cursor: 'pointer' }}
+                onClick={() => setSelectedTaskAmount(taskAmount)}
+              >
+                {/* <Td>{idx + 1}</Td> */}
+                <Td>{taskAmount.id}</Td>
+                <Td>{taskAmount.vehicle?.registration_number ?? '-'}</Td>
+                <Td>{taskAmount.timestamp}</Td>
+                <Td>{taskAmount.work_type ?? '-'}</Td>
+                <Td>{taskAmount.deal_type ?? '-'}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <TaskAmountItemTable taskAmountItems={taskAmount.amount_item} />
+        </>
+      ))}
+
     </>
   )
 
