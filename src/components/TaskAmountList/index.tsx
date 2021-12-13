70d2735974ce97dc308862712f5db238c589c49b
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-12-14 00:43:04 +0530

    Connect AddAmountModal to TakAmountList

--- src/components/TaskAmountList/index.tsx
+++ src/components/TaskAmountList/index.tsx
@@ -1,11 +1,16 @@
 import * as React from "react"
 import {
-  Table, Thead, Tbody, Tr, Th, Td, HStack, Heading, Button
+  Table, Thead, Tbody, Tr, Th, Td, HStack, Heading, Button, useToast
 } from "@chakra-ui/react"
 import { useState } from "react"
 import { ITaskAmount } from "../../models/taskAmount"
 import { TaskAmountDetailModal } from "../TaskAmountDetailModal"
 import { ITaskAmountItem } from "../../models/taskAmountItem"
+import { TaskAmountDeleteConfirmationModal } from "./components/TaskAmountDeleteConfirmationModal"
+import { AddAmountModal } from "./components/AddAmountModal"
+import { handleFetchUpdatedTaskRoute } from "../../store/thunks/TaskRoute"
+import { deleteTaskAmount } from "../../services/apiRequests/taskAmounts"
+import { ITaskRoute } from "../../models/taskRoute"
 
 const TaskAmountItemTable = ({ taskAmountItems }: { taskAmountItems: Array<ITaskAmountItem> }) => {
   return (
@@ -42,21 +47,64 @@ const TaskAmountItemTable = ({ taskAmountItems }: { taskAmountItems: Array<ITask
 export const TaskAmountList = ({ amountsList, taskRoute }: { amountsList: ITaskAmount[], taskRoute: ITaskRoute }) => {
   const [selectedTaskAmount, setSelectedTaskAmount] = useState<ITaskAmount | undefined>(undefined)
 
+  const toast = useToast();
+
+  const [isAddAmountModalOpen, setAddAmountModalOpen] = useState(false)
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+
+  const addAmountModalOpen = () => setAddAmountModalOpen(true)
+
+  const cancelRef = React.useRef()
+
+  const onDelete = async () => {
+    if (selectedTaskAmount !== null) {
+      try {
+        await deleteTaskAmount(selectedTaskAmount!.id)
+        handleFetchUpdatedTaskRoute(taskRoute.id)
+        toast({
+          title: "Task amount deleted",
+          description: "",
+        })
+      }
+      catch {
+        toast({
+          title: "Error deleting task amount",
+          description: "please try again",
+          status: "error",
+        })
+      }
+    }
+    onDeleteModalClose()
+  }
+
+  const onDeleteModalClose = () => {
+    setSelectedTaskAmount(undefined)
+    setIsDeleteModalOpen(false)
+  }
+
+  const onEditModalClose = () => {
+    setSelectedTaskAmount(undefined)
+    setAddAmountModalOpen(false)
+  }
+
   return (
     <>
       <TaskAmountDetailModal
-        isOpen={!!selectedTaskAmount}
+        isOpen={!!selectedTaskAmount && !isAddAmountModalOpen && !isDeleteModalOpen}
         onClose={() => setSelectedTaskAmount(undefined)}
         taskAmount={selectedTaskAmount}
       />
+      <TaskAmountDeleteConfirmationModal onAccept={onDelete} cancelRef={cancelRef} onCancel={onDeleteModalClose} isOpen={isDeleteModalOpen} />
+      <AddAmountModal selectedTaskAmount={selectedTaskAmount} taskRoute={taskRoute} isOpen={isAddAmountModalOpen} onClose={onEditModalClose} />
+
       <HStack my={6} justifyContent='space-between' >
         <Heading size='lg' textAlign='start' >Amounts</Heading>
-        <Button onClick={() => { }} >Add Amounts</Button>
+        <Button onClick={addAmountModalOpen} >Add Amounts</Button>
       </HStack>
 
       {amountsList?.map((taskAmount: ITaskAmount, idx: number) => (
         <>
-          <Table border='solid 2px' borderColor='blue.600' size="sm" variant='unstyled' bgColor='blue.100' >
+          <Table mt={10} size="sm" variant='unstyled' bgColor='blue.100' >
             <Thead>
               <Tr>
                 {/* <Th>S No.</Th> */}
